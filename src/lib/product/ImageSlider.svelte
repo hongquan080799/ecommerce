570cda4c3380eeb12093ac0a50fcfc7e2a3b1b3e
HongQuan <hongquan080799@gmail.com>
2024-04-10 01:53:08 +0700

    carousel product

--- src/lib/product/ImageSlider.svelte
+++ src/lib/product/ImageSlider.svelte
@@ -17,28 +17,23 @@
 			title: image
 		};
 	});
+	console.log(images);
 	const scaleAnimation = (x) => scale(x, { duration: 500, easing: quintOut });
 </script>
 
 <Button color={'blue'} on:click={() => (formModal = true)}>Xem</Button>
 
-<Modal
-	title="Hình ảnh sản phẩm"
-	bind:open={formModal}
-	size="xl"
-	autoclose={false}
-	class="min-h-96 w-full"
->
-	<div class="relative mt-5 overflow-x-auto shadow-md sm:rounded-lg">
-		<div class="flex justify-between bg-white pb-4 dark:bg-gray-900">
-			<div class="max-w-4xl">
-				{#if images.length > 0}
-					<Carousel {images} transition={scaleAnimation} let:Controls let:Indicators>
-						<Controls />
-						<Indicators />
-					</Carousel>
-				{/if}
-			</div>
-		</div>
+<Modal title="Hình ảnh sản phẩm" bind:open={formModal} size="md" autoclose={false} class=" w-full">
+	<div class=" mx-auto max-w-2xl bg-slate-500">
+		<Carousel
+			{images}
+			transition={scaleAnimation}
+			let:Controls
+			let:Indicators
+			imgClass="object-contain h-full w-fit"
+		>
+			<Controls />
+			<Indicators />
+		</Carousel>
 	</div>
 </Modal>
