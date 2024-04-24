a399545dd31fcb7ce16c7df6a2dc4bd14991e5ad
thquan <issacnewton321@gmail.com>
2024-04-24 14:15:26 +0700

    update build

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,22 +1,16 @@
 import { redirect, type Handle } from '@sveltejs/kit';
 import {authenticateUser} from '$lib/authentication/auth'
 export const handle: Handle = async ({ event, resolve }) => {
-    console.log('hook')
     //authen user
     const user = await authenticateUser(event)
-    console.log(user)
     if(user) {
         event.locals.user = user
-        console.log('1')
     }
     if (event.url.pathname.startsWith('/admin')) {	
-        console.log('2')
         if(!user) {
-            console.log('3')
             redirect(303, '/')
         }
         if (user.role.name !== 'ADMIN') {
-            console.log('')
             redirect(303, '/')
         }
     }
---
+++ src/routes/authentication/login/+page.server.ts
@@ -0,0 +1,25 @@
+import * as api from '$api';
+import { redirect, type Actions } from "@sveltejs/kit";
+
+
+export const actions: Actions = {
+    default: async ({cookies, request}) => {
+        const data = await request.formData();
+        const username = data.get('username');
+        const password = data.get('password');
+        const response = await api.userApi.login({username, password});
+        cookies.set("jwt", response.jwtToken, {
+			path: "/",
+			httpOnly: true,
+			sameSite: "strict",
+			secure: process.env.NODE_ENV === "production",
+			maxAge: 60 * 60 * 24 * 7, // 1 week
+		})
+        if(response.userInfo.role.name === "ADMIN") {
+            redirect(303, '/admin')
+        }
+        return { success: true };
+        
+    }
+} satisfies Actions;
+export const prerender = false;
